9e1cec600b61bb7fa0e960b972a0c50089b8215e
narmat998 <narmat9001@gmail.com>
2026-09-23 15:25:53 -0600

    scene

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,11 +5,89 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x;
+let y;
+
+let dx;
+let dy;
+
+let speed;
+
+let stage;
 
 async function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);;
+  noStroke();
+  rectMode(CENTER);
+  x = width/2;
+  y = height*2/3;
+  speed = 8;
+  stage = 1;
 }
 
 function draw() {
-  background(220);
+
+  makeCharacter();
+  moveCharacter();
+  checkStage();
 }
+
+function makeCharacter() {
+  fill('blue');
+  rect(x, y, 60, 40);
+}
+
+
+function moveCharacter() {
+  // if (keyIsDown('w') || keyIsDown(UP_ARROW)) {
+  //   y -= speed;
+  // }
+  // if (keyIsDown('s') || keyIsDown(DOWN_ARROW)) {
+  //   y += speed;
+  // }
+
+  if (keyIsDown('d') || keyIsDown(RIGHT_ARROW)) {
+    x += speed;
+  }
+  if (keyIsDown('a') || keyIsDown(LEFT_ARROW)) {
+    x -= speed;
+  }
+}
+
+function keyPressed(){
+  if (key === 'g') {
+    
+  }
+}
+
+function checkStage() {
+  if (stage === 1) {
+    background('white');
+    x = width/2;
+    y = height*2/3;
+    if (x >= width + 20) {
+      stage = 2;
+    }
+    else if (x <= width - 20) {
+      stage = 3;
+    }
+  }
+  else if (stage === 2) {
+    background('red');
+    x = width/4;
+    y = height*2/3;
+    if (x <= width - 20) {
+      stage = 1;
+    }
+  }
+  else if (stage === 3) {
+    background('green');
+    x = width * 3/4;
+    y = height*2/3;
+  }
+
+
+}
+
+
+
